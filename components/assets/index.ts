cca62289e5d09209871b1db80226014752a6f3cc
AnxForever <130662349+AnxForever@users.noreply.github.com>
2026-03-16 13:54:34 +0800

    chore: remove broken assets page and related code

--- components/assets/index.ts
+++
@@ -1,7 +0,0 @@
-export { AssetCard } from "./asset-card";
-export { AssetGallery } from "./asset-gallery";
-export { AssetPicker } from "./asset-picker";
-export { AssetButton } from "./asset-button";
-export { AssetBadge } from "./asset-badge";
-export { AssetDecoration } from "./asset-decoration";
-export { AssetDownloadDialog } from "./asset-download-dialog";
