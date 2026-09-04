c0fa8f9f10d94e3f804c76c0bb69ece0d0b81e23
AnxForever <130662349+AnxForever@users.noreply.github.com>
2026-09-04 09:09:09 +0800

    refactor(styles): expose style taxonomies and data-level a11y scoring

--- lib/accessibility/index.ts
+++ lib/accessibility/index.ts
@@ -1,3 +1,3 @@
 export { hexToRgb, relativeLuminance, contrastRatio, meetsAA, meetsAAA, extractHexFromClass } from "./wcag";
-export { scoreStyle, scoreAllStyles } from "./scorer";
+export { scoreStyle, scoreStyleData, scoreAllStyles } from "./scorer";
 export type { AccessibilityScore, ContrastScore, ReadabilityScore, ColorPair } from "./scorer";
